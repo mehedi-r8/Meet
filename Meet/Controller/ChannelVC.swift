c4321f68b6e6d2c94f2ae760e45cee1cdd738fe1
MEHEDI.R8 <mehedi.r8@MEHEDIR8s-MacBook-Air.local>
2018-09-11 10:15:16 +0600

    slide menu added

---
+++ Meet/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Meet
+//
+//  Created by MEHEDI.R8 on 9/11/18.
+//  Copyright © 2018 mehedi.r8. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+    }
+    
+
+}
---
+++ Meet/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Meet
+//
+//  Created by MEHEDI.R8 on 9/11/18.
+//  Copyright © 2018 mehedi.r8. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
+
+    }
+    
+
+
+
+}
